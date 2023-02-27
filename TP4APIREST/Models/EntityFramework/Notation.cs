f90cfc138ef37d17d414456f72cd99bf6c01e39d
cherifad <cherifad@D250-S09>
2023-02-27 10:12:18 +0100

    Ajoutez des fichiers projet.

---
+++ TP4APIREST/Models/EntityFramework/Notation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace TP4APIREST.Models.EntityFramework
+{
+    [Table("t_j_notation_not")]
+    public partial class Notation
+    {
+        public Notation()
+        {
+        }
+
+        [ForeignKey("fk_not_utl")]
+        [Column("utl_id", Order = 0)]
+        public int UtilisateurId { get; set; }
+
+        [ForeignKey("fk_not_flm")]
+        [Column("flm_id", Order = 1)]
+        public int FilmId { get; set; }
+
+        [Column("not_note")]
+        public int? Note { get; set; } = null!;
+
+        [InverseProperty("Notations")]
+        public virtual Film FilmNavigation { get; set; } = null!;
+
+        [InverseProperty("NotesUtilisateur")]
+        public virtual Utilisateur UtilisateurNavigation { get; set; } = null!;
+    }
+}
