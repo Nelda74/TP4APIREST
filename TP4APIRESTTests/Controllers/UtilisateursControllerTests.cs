afa1b73e7168fd6d0c00ba37c1ab99c88c4f718d
cherifad <cherifad@D250-S09>
2023-02-27 11:45:28 +0100

    test

---
+++ TP4APIRESTTests/Controllers/UtilisateursControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP4APIREST.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP4APIREST.Controllers.Tests
+{
+    [TestClass()]
+    public class UtilisateursControllerTests
+    {
+        [TestMethod()]
+        public void UtilisateursControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUtilisateursTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUtilisateurByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUtilisateurByEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutUtilisateurTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostUtilisateurTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteUtilisateurTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
